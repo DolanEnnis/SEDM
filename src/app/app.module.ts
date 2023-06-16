7298fda356d302459cbf0b1ce58e8a5d3cf24f66
Brian Dolan <dolan.ennis@gmail.com>
2023-06-16 19:03:22 +0100

    Has firebase errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAEZ85Q2v4zFULXdqbPaW8TWGOfSRo7Hk8',
+    authDomain: 'sedm-bd018.firebaseapp.com',
+    databaseURL: 'https://sedm-bd018.firebaseio.com',
+    projectId: 'sedm-bd018',
+    storageBucket: 'sedm-bd018.appspot.com',
+    messagingSenderId: '770876376231',
+    appId: '1:770876376231:web:775470d39603163920c782',
+  },
+};
